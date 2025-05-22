faf07fa44e18ec76c3a1c23c820fbede33456dfa
Nazmul Islam Sakib <sakibsarkar>
2025-05-22 13:13:17 +0600

    feat: Muzahid code fixed

--- src/components/productDetails/ProductDetails.tsx
+++ src/components/productDetails/ProductDetails.tsx
@@ -1,28 +1,31 @@
+import { ProductDetailsProps } from "@/app/(main)/products/[slug]/page";
+import { baseUrl } from "@/redux/api/api";
+import { IProduct } from "@/types/product";
 import { notFound } from "next/navigation";
 import Breadcrumb from "../ui/BreadCrumbs";
 import DetailedInfo from "./DetailedInfo";
-import ProductDetailsSlider from "./ProductDetailSlider";
-import { ProductDetailsProps } from "@/app/(main)/products/[slug]/page";
 import DetailsAndInformation from "./DetailsAndInformation";
+import ProductDetailsSlider from "./ProductDetailSlider";
 import RelatedProducts from "./RelatedProducts";
 
 const ProductDetails = async ({ params }: ProductDetailsProps) => {
   const slug = (await params)?.slug;
 
-  const res = await fetch(`http://localhost:5000/api/v1/product/get/${slug}`, {
+  const res = await fetch(`${baseUrl}/product/get/${slug}`, {
     cache: "no-store",
   });
 
   if (!res.ok) return notFound();
 
-  const product = await res.json();
+  const data = (await res.json()) as { data: IProduct };
+  const product = data?.data;
 
-  console.log(product);
   return (
     <div className="mx-auto max-w-[1756px] px-[16px] py-[10px] transition-all sm:px-[36px] md:px-[50px] lg:px-[95px] 2xl:px-[0px]">
       <Breadcrumb />
+      {product.name}
       <div className="mt-[10px] grid grid-cols-1 gap-[10px] lg:grid-cols-2">
-        <ProductDetailsSlider {...product} />
+        <ProductDetailsSlider product={product} />
         <DetailedInfo product={product} />
       </div>
       <DetailsAndInformation {...product} />
--- src/components/productDetails/RelatedProducts.tsx
+++ src/components/productDetails/RelatedProducts.tsx
@@ -1,6 +1,6 @@
 import { products } from "@/mock/FakeProducts";
+import ProductSecondaryCard from "../ui/Card/ProductCard/ProductSecondaryCard";
 import Title from "../ui/Title";
-import ProductCard from "../Home/Collections/ProductCard";
 
 const RelatedProducts = () => {
   return (
@@ -8,7 +8,9 @@ const RelatedProducts = () => {
       <Title title="RELATED PRODUCTS" className="!text-[14px]" />
 
       <div className="my-[15px] grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-        {products?.slice(0, 6)?.map((item) => <ProductCard key={item._id} product={item} />)}
+        {products
+          ?.slice(0, 6)
+          ?.map((item) => <ProductSecondaryCard key={item._id} product={item} />)}
       </div>
     </div>
   );
