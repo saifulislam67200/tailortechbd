5ba59dc18609d0f5f8e4fa135b8c691a2ea89d13
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-05-24 10:22:38 +0600

    product details page slider

--- src/components/productDetails/ProductDetailSlider.tsx
+++ src/components/productDetails/ProductDetailSlider.tsx
@@ -23,42 +23,44 @@ const ProductDetailsSlider = ({ product }: { product: IProduct }) => {
     <section className="bg-white px-[10px] py-[14px] md:px-[20px]">
       <div className="flex flex-col-reverse items-center lg:flex-row lg:items-start lg:gap-[20px] xl:gap-[30px]">
         {/* thumbnail images ,vertical on desktop, horizontal on mobile */}
-        <div className="mt-[5px] lg:w-[80px]">
-          <Swiper
-            onSwiper={setThumbsSwiper}
-            direction="horizontal"
-            spaceBetween={12}
-            slidesPerView={4}
-            freeMode={true}
-            watchSlidesProgress={true}
-            modules={[FreeMode, Thumbs]}
-            className="thumbnail-slider h-[80px] w-full gap-[90px] overflow-x-scroll lg:h-auto lg:gap-[20px]"
-            breakpoints={{
-              1024: {
-                direction: "vertical",
-                spaceBetween: 16,
-                slidesPerView: 4,
-              },
-            }}
-            onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-          >
-            {images?.map((img, index) => (
-              <SwiperSlide key={index} className="!h-[80px] !w-[80px]">
-                <div
-                  className={`relative h-[60px] w-[60px] cursor-pointer overflow-hidden border p-[5px] transition-all duration-300 md:h-[80px] md:w-[80px] ${activeIndex === index ? "border-info-light" : "border-transparent"}`}
-                >
-                  <Image
-                    src={img}
-                    width={80}
-                    height={200}
-                    className="h-full w-full object-cover"
-                    alt={`Thumbnail ${index + 1}`}
-                    priority={index < 2}
-                  />
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+        <div className="mt-[5px] h-fit w-full overflow-hidden md:max-h-[350px] lg:w-fit 2xl:max-h-[655px]">
+          <div className="overflow-x-auto lg:w-[80px] lg:overflow-x-hidden lg:overflow-y-auto">
+            <Swiper
+              onSwiper={setThumbsSwiper}
+              direction="horizontal"
+              spaceBetween={4}
+              slidesPerView={4}
+              freeMode={true}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Thumbs]}
+              className="thumbnail-slider h-[80px] w-full gap-[90px] overflow-x-scroll lg:h-auto lg:gap-[20px]"
+              breakpoints={{
+                1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 16,
+                  direction: "vertical",
+                },
+              }}
+              onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+            >
+              {images?.map((img, index) => (
+                <SwiperSlide key={index} className="!h-[80px] !w-[80px]">
+                  <div
+                    className={`relative h-[60px] w-[60px] cursor-pointer overflow-hidden border p-[5px] transition-all duration-300 md:h-[80px] md:w-[80px] ${activeIndex === index ? "border-info-light" : "border-transparent"}`}
+                  >
+                    <Image
+                      src={img}
+                      width={80}
+                      height={200}
+                      className="h-full w-full object-cover"
+                      alt={`Thumbnail ${index + 1}`}
+                      priority={index < 2}
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
 
         {/* main Image */}
