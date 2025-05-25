554c18f094565dcf9a845867a352871e0c5b1654
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2025-05-25 15:30:41 +0600

    update banner section

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,33 +1,41 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import type { Swiper as SwiperType } from "swiper";
 import "swiper/css";
 
 const Hero = () => {
   const [activeSlide, setActiveSlide] = useState(0);
+  const swiperRef = useRef<SwiperType | null>(null);
 
-  const leftImages = ["/images/home/banner/banner1.png", "/images/home/banner/banner2.png", "/images/home/banner/banner3.png",];
+  const leftImages = [
+    "/images/home/banner/banner1.png",
+    "/images/home/banner/banner2.png",
+    "/images/home/banner/banner3.png",
+  ];
 
   return (
     <div className="mt-[16px]">
       <div className="flex flex-col gap-[8px] lg:flex-row">
         {/* Left Side - Main Banner */}
-        <div className="w-full">
-          <div className="relative mb-[4px] aspect-[834.66/250] w-full">
+        <div className="w-full bg-white">
+          <div className="relative mb-[8px] aspect-[834.66/407] w-full lg:h-[434.7px]">
             <Swiper
+              loop={true}
+              speed={600}
+              allowTouchMove={false}
+              onSwiper={(swiper) => (swiperRef.current = swiper)}
               style={{ width: "100%", height: "100%" }}
               spaceBetween={20}
-              centeredSlides={true}
               autoplay={{
                 delay: 5000,
                 disableOnInteraction: false,
               }}
               modules={[Autoplay]}
-              onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
+              onSlideChange={(swiper) => setActiveSlide(swiper.realIndex)}
             >
               {leftImages.map((image, index) => (
                 <SwiperSlide key={index}>
@@ -46,12 +54,17 @@ const Hero = () => {
           </div>
 
           {/* Indicators */}
-          <div className="flex justify-center gap-[8px] bg-white">
+          <div className="flex justify-center gap-[8px]">
             {leftImages.map((_, index) => (
               <button
                 key={index}
-                onClick={() => setActiveSlide(index)}
-                className={`h-[3px] w-[30px] lg:h-[3px] lg:w-[40px] ${activeSlide === index ? "bg-black" : "bg-[#878787]"}`}
+                onClick={() => {
+                  setActiveSlide(index);
+                  swiperRef.current?.slideToLoop(index);
+                }}
+                className={`h-[3px] w-[30px] cursor-pointer lg:h-[3px] lg:w-[40px] ${
+                  activeSlide === index ? "bg-black" : "bg-[#878787]"
+                }`}
                 aria-label={`slide-${index + 1}`}
               />
             ))}
