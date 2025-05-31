315b4c1d4a386514a1e9b77d71349098abbe69e6
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-05-31 14:40:31 +0600

    quick size chart dynamic, feature done

--- src/components/Dashboard/Product/ProductSizeInput.tsx
+++ src/components/Dashboard/Product/ProductSizeInput.tsx
@@ -3,6 +3,7 @@ import { useGetAllSizeChartQuery } from "@/redux/features/productSizeChart/produ
 import React, { useState } from "react";
 import AddNewProductSizeChart from "./AddNewProductSizeChart";
 import { LiaEdit } from "react-icons/lia";
+import DeleteSizeChartById from "./DeleteSizeChartById";
 interface IProps {
   defaultValue?: string[][];
   onChange?: (value: string[][]) => void;
@@ -11,9 +12,11 @@ interface IProps {
 interface ISizeChart {
   label: string;
   chart: string[][];
+  _id: string;
 }
 
 const ProductSizeInput: React.FC<IProps> = ({ defaultValue, onChange }) => {
+  const [isEdit, setIsEdit] = useState(false);
   const { data } = useGetAllSizeChartQuery(undefined);
   const productSizeChart = data?.data || [];
   const [selectedSize, setSelectedSize] = useState<
@@ -31,22 +34,32 @@ const ProductSizeInput: React.FC<IProps> = ({ defaultValue, onChange }) => {
       <div className="flex flex-col gap-[5px]">
         <div className="flex items-center text-[12px] font-[700]">
           Quick size select:{" "}
-          <button type="button" className="cursor-pointer p-[5px]">
-            <LiaEdit size={16} />
+          <button
+            title="Click to edit quick chart"
+            onClick={() => setIsEdit(!isEdit)}
+            type="button"
+            className="cursor-pointer p-[5px]"
+          >
+            <LiaEdit size={18} />
           </button>
         </div>
         <div className="flex items-center justify-center gap-[10px]">
-          {productSizeChart?.map(({ chart, label }: ISizeChart) => (
-            <button
-              type="button"
-              onClick={() => setSelectedSize({ chart, label })}
-              key={label}
-              className={`cursor-pointer border-[1px] px-[8px] py-[4px] text-[12px] text-primary ${
-                selectedSize?.label === label ? "bg-primary text-white" : "bg-white text-primary"
-              }`}
-            >
-              {label}
-            </button>
+          {productSizeChart?.map(({ chart, label, _id }: ISizeChart) => (
+            <div key={_id} className="flex">
+              <button
+                type="button"
+                onClick={() => setSelectedSize({ chart, label })}
+                disabled={isEdit}
+                className={`${isEdit ? "cursor-not-allowed" : "cursor-pointer"} items-enter flex items-center-safe border-[1px] border-primary px-[8px] py-[4px] text-[12px] text-primary ${
+                  selectedSize?.label === label ? "bg-primary text-white" : "bg-white text-primary"
+                }`}
+              >
+                {label}
+              </button>
+              {isEdit && (
+                <DeleteSizeChartById chartLabel={label} sizeChartId={_id} setIsEdit={setIsEdit} />
+              )}
+            </div>
           ))}
           <AddNewProductSizeChart />
         </div>
