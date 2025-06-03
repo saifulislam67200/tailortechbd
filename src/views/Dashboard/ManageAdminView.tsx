7ad3983a8554044a769a9c8e27363f2460ee5c0a
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-06-03 22:14:20 +0600

    DashboardPageHeadingTitle add

--- src/views/Dashboard/ManageAdminView.tsx
+++ src/views/Dashboard/ManageAdminView.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import DashboardPageHeadingTitle from "@/components/Dashboard/DashboardPageHeadingTitle";
 import AllAdminTable from "@/components/Dashboard/ManageAdmins/AllAdminTable";
 
 const ManageAdminView = () => {
-  return <AllAdminTable />;
+  return (
+    <div className="flex flex-col gap-[10px]">
+      <DashboardPageHeadingTitle title="Mange Admins" />
+      <AllAdminTable />
+    </div>
+  );
 };
 
 export default ManageAdminView;
--- src/views/Dashboard/ManageCustomerView.tsx
+++ src/views/Dashboard/ManageCustomerView.tsx
@@ -1,8 +1,14 @@
 "use client";
+import DashboardPageHeadingTitle from "@/components/Dashboard/DashboardPageHeadingTitle";
 import AllCustomerTable from "@/components/Dashboard/manageCustomer/AllCustomerTable";
 
 const ManageCustomerView = () => {
-  return <AllCustomerTable />;
+  return (
+    <div className="flex flex-col gap-[10px]">
+      <DashboardPageHeadingTitle title="Mange Customers" />
+      <AllCustomerTable />
+    </div>
+  );
 };
 
 export default ManageCustomerView;
