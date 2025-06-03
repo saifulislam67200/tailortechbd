955fd2ca0b4b33622d7debd149d2c41004abf12d
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-06-03 15:49:21 +0600

    input.tsx update

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,20 +1,49 @@
-import { forwardRef, InputHTMLAttributes } from "react";
+"use client";
+
+import { forwardRef, useState } from "react";
+import type { InputHTMLAttributes } from "react";
+import { IoMdEyeOff } from "react-icons/io";
+import { MdRemoveRedEye } from "react-icons/md";
 import { twMerge } from "tailwind-merge";
+
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
-  return (
-    <input
-      ref={ref}
-      className={twMerge(
-        "w-full appearance-none border-[1px] border-border-main bg-white bg-clip-padding px-[12px] py-[6px] text-base text-[12px] font-normal text-strong outline-none",
-        className
-      )}
-      {...props}
-    />
-  );
-});
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ type = "text", className, ...props }, ref) => {
+    const [showPass, setShowPass] = useState(false);
+    const isPassword = type === "password";
+    const inputType = isPassword && showPass ? "text" : type;
 
-Input.displayName = "Input"; // to avoid issues in React DevTools
+    const inputElement = (
+      <input
+        ref={ref}
+        type={inputType}
+        className={twMerge(
+          "w-full appearance-none border-[1px] border-border-main bg-white bg-clip-padding px-[12px] py-[6px] text-base text-[12px] font-normal text-strong outline-none",
+          className
+        )}
+        {...props}
+      />
+    );
+
+    return isPassword ? (
+      <div className="relative">
+        {inputElement}
+        <button
+          type="button"
+          onClick={() => setShowPass(!showPass)}
+          className="absolute right-3 bottom-2.5 cursor-pointer"
+          tabIndex={-1}
+        >
+          {showPass ? <IoMdEyeOff /> : <MdRemoveRedEye />}
+        </button>
+      </div>
+    ) : (
+      inputElement
+    );
+  }
+);
+
+Input.displayName = "Input";
 
 export default Input;
