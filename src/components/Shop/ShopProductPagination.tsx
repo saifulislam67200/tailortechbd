618a18ec7d3f37e023112d257385b1382e7c8021
Nazmul Islam Sakib <sakibsarkar>
2025-06-04 14:02:52 +0600

    feat: Update Pagination

--- src/components/Shop/ShopProductPagination.tsx
+++ src/components/Shop/ShopProductPagination.tsx
@@ -8,6 +8,7 @@ const ShopProductPagination = ({ totalDoc }: { totalDoc: number }) => {
     <div className="mt-[20px] pb-[20px]">
       <Pagination
         totalDocs={totalDoc}
+        limit={12}
         onPageChange={(page) => updateSearchParams({ page: String(page) })}
       />
     </div>
