43d310af390722aaf98e8aaf979afb135229314a
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-06-29 18:40:37 +0600

    update

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
     template: "%s | TailorTech - Your Style Is Your Story",
   },
   description:
-    "Welcome to TailorTech – Where Tradition Meets Innovation.\nAt Tilortech, we believe fashion is more than just clothing — it's a statement of identity. Our brand blends modern design with timeless comfort, offering high-quality apparel for individuals who value style, authenticity, and confidence. We also blend the timeless art of tailoring with cutting-edge fashion technology to bring you modern, perfectly fitted, and stylish apparel. Every piece is crafted with precision, comfort, and confidence in mind.\nTailorTech is more than a brand — it’s a movement towards smarter fashion.\nJoin us and wear the future.",
+    "Welcome to TailorTech – Where Tradition Meets Innovation.\nAt Tailortech, we believe fashion is more than just clothing — it's a statement of identity. Our brand blends modern design with timeless comfort, offering high-quality apparel for individuals who value style, authenticity, and confidence. We also blend the timeless art of tailoring with cutting-edge fashion technology to bring you modern, perfectly fitted, and stylish apparel. Every piece is crafted with precision, comfort, and confidence in mind.\nTailorTech is more than a brand — it’s a movement towards smarter fashion.\nJoin us and wear the future.",
 
   keywords: ["fashion", "apparel", "tailoring", "style"],
 
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "TailorTech - Your Style Is Your Story",
     description:
-      "Welcome to TailorTech – Where Tradition Meets Innovation.\nAt Tilortech, we believe fashion is more than just clothing — it's a statement of identity. Our brand blends modern design with timeless comfort, offering high-quality apparel for individuals who value style, authenticity, and confidence. We also blend the timeless art of tailoring with cutting-edge fashion technology to bring you modern, perfectly fitted, and stylish apparel. Every piece is crafted with precision, comfort, and confidence in mind.\nTailorTech is more than a brand — it’s a movement towards smarter fashion.\nJoin us and wear the future.",
+      "Welcome to TailorTech – Where Tradition Meets Innovation.\nAt Tailortech, we believe fashion is more than just clothing — it's a statement of identity. Our brand blends modern design with timeless comfort, offering high-quality apparel for individuals who value style, authenticity, and confidence. We also blend the timeless art of tailoring with cutting-edge fashion technology to bring you modern, perfectly fitted, and stylish apparel. Every piece is crafted with precision, comfort, and confidence in mind.\nTailorTech is more than a brand — it’s a movement towards smarter fashion.\nJoin us and wear the future.",
     url: "https://www.bd-shop.com",
     siteName: "TailorTech",
     images: [
