9e922987211c56947a80712589d6ac8f238d9319
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-06-30 15:44:41 +0600

    update

--- src/components/productDetails/DetailsInfoActions.tsx
+++ src/components/productDetails/DetailsInfoActions.tsx
@@ -102,67 +102,77 @@ const DetailsInfoActions: React.FC<IProps> = ({ product, onColorChange }) => {
 
   return (
     <div>
-      {/* colors  */}
-      <h1 className="mt-[10px] text-[16px]">Colors:</h1>
-      <div className="mt-[5px] flex items-center gap-[10px]">
-        {product?.colors?.map((color) => {
-          return (
+      <div className="mt-[15px] flex flex-col gap-[10px] sm:flex-row sm:items-end-safe">
+        <div>
+          {/* colors  */}
+          <h1 className="text-[16px]">Colors:</h1>
+          <div className="mt-[5px] flex items-center gap-[10px]">
+            {product?.colors?.map((color) => {
+              return (
+                <button
+                  key={color._id}
+                  type="button"
+                  aria-label={`Select color ${color.color}`}
+                  className={`flex h-[30px] w-fit cursor-pointer items-center rounded-full border-[1px] border-primary px-[8px] text-[12px] transition-all duration-200 ${
+                    activeColor?.color === color.color
+                      ? "bg-primary text-white"
+                      : "bg-white text-primary"
+                  }`}
+                  onClick={() => handleColorChange(color)}
+                >
+                  {color.color}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          {/* // sizes  */}
+          <h1 className="text-[16px]">Sizes:</h1>
+          <div className="mt-[5px] flex items-center gap-[10px]">
+            {activeColor?.sizes?.map((size) => (
+              <button
+                key={size._id}
+                type="button"
+                aria-label={`Select size ${size.size}`}
+                className={`h-[30px] w-fit cursor-pointer px-[8px] text-[12px] font-medium transition-all duration-200 ${
+                  activeSize?.size === size.size
+                    ? "bg-primary text-white shadow-none"
+                    : "bg-white text-black shadow"
+                } border border-gray-200 hover:bg-primary hover:text-white`}
+                onClick={() => handleSizeChange(size)}
+              >
+                {size.size}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div>
+          {/* // quantity update  */}
+          <h1 className="text-[16px]">Quantity:</h1>
+
+          <div className="flex h-[30px] w-[80px] items-center border border-quaternary px-[7px]">
             <button
-              key={color._id}
-              type="button"
-              aria-label={`Select color ${color.color}`}
-              className={`flex h-[20px] w-fit cursor-pointer items-center rounded-full border-[1px] border-primary px-[8px] text-[12px] transition-all duration-200 ${
-                activeColor?.color === color.color
-                  ? "bg-primary text-white"
-                  : "bg-white text-primary"
-              }`}
-              onClick={() => handleColorChange(color)}
+              disabled={activeQuantity <= 1}
+              onClick={() => handleQuantityChange("dec")}
+              className={`cursor-pointer text-info ${activeQuantity <= 1 ? "cursor-event-none" : ""}`}
             >
-              {color.color}
+              {activeQuantity > 1 ? (
+                <AiOutlineMinus size={14} />
+              ) : (
+                <AiOutlineMinus size={14} className="text-gray-300" />
+              )}
             </button>
-          );
-        })}
-      </div>
-      {/* // sizes  */}
-      <h1 className="mt-[15px] text-[16px]">Sizes:</h1>
-      <div className="mt-[5px] flex items-center gap-[10px]">
-        {activeColor?.sizes?.map((size) => (
-          <button
-            key={size._id}
-            type="button"
-            aria-label={`Select size ${size.size}`}
-            className={`h-[30px] w-fit cursor-pointer px-[8px] text-[12px] font-medium transition-all duration-200 ${
-              activeSize?.size === size.size
-                ? "bg-primary text-white shadow-none"
-                : "bg-white text-black shadow"
-            } border border-gray-200 hover:bg-primary hover:text-white`}
-            onClick={() => handleSizeChange(size)}
-          >
-            {size.size}
-          </button>
-        ))}
-      </div>
-      {/* // quantity update  */}
-      <div className="mt-[15px] flex h-[30px] w-[80px] items-center border border-quaternary px-[7px]">
-        <button
-          disabled={activeQuantity <= 1}
-          onClick={() => handleQuantityChange("dec")}
-          className={`cursor-pointer text-info ${activeQuantity <= 1 ? "cursor-event-none" : ""}`}
-        >
-          {activeQuantity > 1 ? (
-            <AiOutlineMinus size={14} />
-          ) : (
-            <AiOutlineMinus size={14} className="text-gray-300" />
-          )}
-        </button>
-        <p className="w-full text-center text-[14px]">{activeQuantity}</p>
-        <button
-          disabled={activeSize?.stock === 0 || activeSize?.stock === activeQuantity}
-          onClick={() => handleQuantityChange("inc")}
-          className="cursor-pointer text-info disabled:cursor-not-allowed disabled:opacity-[0.5]"
-        >
-          <AiOutlinePlus size={14} />
-        </button>
+            <p className="w-full text-center text-[14px]">{activeQuantity}</p>
+            <button
+              disabled={activeSize?.stock === 0 || activeSize?.stock === activeQuantity}
+              onClick={() => handleQuantityChange("inc")}
+              className="cursor-pointer text-info disabled:cursor-not-allowed disabled:opacity-[0.5]"
+            >
+              <AiOutlinePlus size={14} />
+            </button>
+          </div>
+        </div>
       </div>
 
       {activeSize && !activeSize.stock ? (
@@ -180,7 +190,7 @@ const DetailsInfoActions: React.FC<IProps> = ({ product, onColorChange }) => {
           productId={product?._id}
         />
       ) : (
-        <div className="mt-[10px] flex flex-col items-center gap-[10px] sm:max-w-[330px] sm:flex-row">
+        <div className="mt-[20px] flex flex-col items-center gap-[10px] sm:max-w-[330px] sm:flex-row">
           <button
             disabled={!activeColor || !activeSize || !activeSize.stock}
             onClick={handleAddToCart}
