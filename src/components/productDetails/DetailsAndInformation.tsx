11c648d27493c2783c03c57fb3624f5ce0e967f8
midul <midulislamrasel01@gmail.com>
2025-09-06 00:58:06 +0600

    ad

--- src/components/productDetails/DetailsAndInformation.tsx
+++ src/components/productDetails/DetailsAndInformation.tsx
@@ -6,25 +6,27 @@ import ScrollDetailsSection from "./ScrollDetailsSection";
 import RelatedProducts from "./RelatedProducts";
 const DetailsAndInformation = ({ product, slug }: { product: IProduct; slug: string }) => {
   return (
-<section >
-  <p className="text-[14px] mt-[15px]"><span className="font-bold">NB:</span>The actual color of the physical product may slightly variation due to the deviation of lighting sources, photography or your device display settings.
-September 6, 2025
-</p>
+    <section>
+      <p className="mt-[15px] text-[14px]">
+        <span className="font-bold">NB:</span>The actual color of the physical product may slightly
+        variation due to the deviation of lighting sources, photography or your device
+        display settings. September 6, 2025
+      </p>
       <div className="mt-[15px] flex flex-col gap-[13px] md:flex-row">
-      <div className="w-full">
-        <ScrollDetailsSection />
-        <div
-          id="product-description"
-          className="border border-quaternary bg-white p-[20px] text-black"
-          dangerouslySetInnerHTML={{ __html: product?.description || "" }}
-        ></div>
-        <QuestionAnswer productId={product?._id} />
-        <CustomerReview productId={product?._id} />
-      </div>
+        <div className="w-full">
+          <ScrollDetailsSection />
+          <div
+            id="product-description"
+            className="border border-quaternary bg-white p-[20px] text-black"
+            dangerouslySetInnerHTML={{ __html: product?.description || "" }}
+          ></div>
+          <QuestionAnswer productId={product?._id} />
+          <CustomerReview productId={product?._id} />
+        </div>
 
-      <RelatedProducts slug={slug} />
-    </div>
-</section>
+        <RelatedProducts slug={slug} />
+      </div>
+    </section>
   );
 };
 
